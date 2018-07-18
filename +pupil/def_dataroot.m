b47a290201d69f0a4cb6c63c1c936e4a4edf441f
nfagan <nick@fagan.org>
2018-07-18 12:06:32 -0400

    add folder maker

--- +pupil/def_dataroot.m
+++ +pupil/def_dataroot.m
@@ -4,6 +4,8 @@ assert( ischar(dr), 'Path must be char.' );
 
 pathsdir = pupil.get_pathsdir();
 
+if ( exist(pathsdir, 'dir') ~= 7 ), mkdir( pathsdir ); end
+
 save( fullfile(pathsdir, 'dataroot.mat'), 'dr' );
 
 end
